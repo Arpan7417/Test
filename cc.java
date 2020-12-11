b2defa349fd3625438aca7cc04c10228bf4b8564
Arpan7417 <arpansurabhi@gmail.com>
2020-12-11 00:21:39 +0000

    Java Ide

---
+++ cc.java
@@ -0,0 +1,33 @@
+interface Inf1{
+   public void methodx();
+   public void methody();
+   public void methodz();
+   
+}
+interface Inf2 extends Inf1 {
+   public void method2();
+}
+public class cc implements Inf2{
+   /* Even though this class is only implementing the
+    * interface Inf2, it has to implement all the methods 
+    * of Inf1 as well because the interface Inf2 extends Inf1*/
+    
+    public void methodx(){
+	System.out.println(" hello x");
+    }
+    public void methody(){
+	System.out.println(" hello y");
+    }
+    public void methodz(){
+	System.out.println(" hello z");
+    }
+    public void method2(){
+	System.out.println("method2");
+    }
+    public static void main(String args[]){
+	Inf1 obj = new cc();
+	obj.methodx();
+	obj.methody();
+	obj.methodz();
+    }
+}
